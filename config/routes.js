122ce4bca9ee38ece99450cf6533a2dcab6be723
Miroslav <fshn1994@abv.bg>
2020-06-10 23:48:17 +0300

    feat:add new accessory pages

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
-const { getAllCubes, getCube } = require('../controllers/cubes')
+const { getAllCubes, getCube, updateCube, getCubeWithAccessories } = require('../controllers/cubes')
+const { getAccessories } = require('../controllers/accessories')
 const Cube = require('../models/cube')
+const Accessory = require('../models/accessory')
 
 module.exports = (app) => {
 
@@ -36,18 +38,69 @@ module.exports = (app) => {
         cube.save((err) => {
             if (err) {
                 console.error(err);
+                res.redirect('/create')
+            } else {
+                res.redirect('/')
             }
-            res.redirect('/')
         })
     })
 
     app.get('/details/:id', async (req, res) => {
-        const cube = await getCube(req.params.id)
+        const cube = await getCubeWithAccessories(req.params.id)
 
         res.render('details', {
             title: 'Details Cube | Cube Workshop',
             ...cube
         })
+    })
+
+    app.get('/create/accessory', (req, res) => {
+        res.render('createAccessory', {
+            title: 'Create Accessory | Cube Workshop'
+        })
+    })
+
+    app.post('/create/accessory', async (req, res) => {
+        const {
+            name,
+            description,
+            imageUrl
+        } = req.body
+        const accessory = new Accessory({ name, description, imageUrl })
+
+        await accessory.save((err) => {
+            if (err) {
+                console.error(err);
+            }
+            res.redirect('/')
+        })
+    })
+
+    app.get('/attach/accessory/:id', async (req, res) => {
+        const cube = await getCube(req.params.id)
+        const accessories = await getAccessories()
+        const canAttachAccessory = cube.accessories.length !== accessories.length && accessories.length > 0
+
+        const cubeAccessories = cube.accessories.map(acc => acc._id.valueOf().toString())
+
+        const notAttachedAccessories = accessories.filter(acc => {
+            const accessoriesToString = acc._id.valueOf().toString()
+            return !cubeAccessories.includes(accessoriesToString)
+        })
+
+        res.render('attachAccessory', {
+            title: 'Attach Accessory | Cube Workshop',
+            ...cube,
+            accessories: notAttachedAccessories,
+            canAttachAccessory
+        })
+    })
+
+    app.post('/attach/accessory/:id', async (req, res) => {
+        const { accessory } = req.body
+        await updateCube(req.params.id, accessory)
+
+        res.redirect(`/details/${req.params.id}`)
     })
 
     // app.get('/delete/:id', (req, res) => {
