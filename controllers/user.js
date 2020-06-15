ba442002a0dd830194588c5da29fa5ac722a0ad6
Miroslav <fshn1994@abv.bg>
2020-06-15 17:45:28 +0300

    feat: finish signup

---
+++ controllers/user.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const privateKey = 'CUBE-WORKSHOP-SOFTUNI'
+
+
+const saveUser = async (req, res) => {
+    // hashing
+    const { username, password } = req.body
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = new User({ username, password: hashedPassword })
+    const userObj = await user.save()
+
+    const token = jwt.sign({
+        userID: userObj._id,
+        username: userObj.username
+    }, privateKey)
+
+    res.cookie('aid', token);
+
+    return true
+}
+
+module.exports = {
+    saveUser
+}
