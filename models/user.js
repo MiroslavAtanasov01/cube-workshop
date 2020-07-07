9fbc8e56352c876cb9bd691754df02572b075272
Miroslav <fshn1994@abv.bg>
2020-07-07 17:03:19 +0300

    feat: remove unnecessary code

--- models/user.js
+++ models/user.js
@@ -12,36 +12,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Password is required!'],
-        match: [/^[A-Za-z0-9]+$/g, 'Password is not valid'],
-        minlength: [3, 'Password is not valid'],
     }
 })
-
-
-// UserSchema.methods = {
-//     matchPassword: function (password) {
-//         return bcrypt.compare(password, this.password);
-//     }
-// };
-
-// UserSchema.pre('save', function (next) {
-//     if (this.isModified('password')) {
-//         bcrypt.genSalt(10, (err, salt) => {
-//             if (err) {
-//                 return next(err)
-//             }
-//             bcrypt.hash(this.password, salt, (err, hash) => {
-//                 if (err) {
-//                     return next(err)
-//                 }
-//                 this.password = hash;
-//                 next();
-//             });
-//         });
-//         return;
-//     }
-//     next();
-// });
-
 
 module.exports = mongoose.model('User', UserSchema)
--- routes/cube.js
+++ routes/cube.js
@@ -4,47 +4,11 @@ const config = require('../config/config')[env]
 const router = require('express')()
 const jwt = require('jsonwebtoken')
 const Cube = require('../models/cube')
-const { authAccess, getUserStatus } = require('../controllers/user')
+const { authAccess } = require('../controllers/user')
 const { getCubeWithAccessories, editCube, deleteCube } = require('../controllers/cubes')
 
-router.get('/edit/:id', authAccess, getUserStatus, async (req, res) => {
-    const cube = await getCubeWithAccessories(req.params.id)
 
-    res.render('editCubePage', {
-        title: 'Edit cube| Cube Workshop',
-        cube,
-    })
-})
-
-router.post('/edit/:id', authAccess, getUserStatus, async (req, res, next) => {
-    const { name, description, imageUrl, difficultyLevel } = req.body
-
-    try {
-        await editCube(req.params.id, { name, description, imageUrl, difficulty: difficultyLevel })
-        res.redirect(`/details/${req.params.id}`)
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.get('/delete/:id', authAccess, getUserStatus, async (req, res) => {
-    const cube = await getCubeWithAccessories(req.params.id)
-    res.render('deleteCubePage', {
-        title: 'deleteCube | Cube Workshop',
-        ...cube,
-    })
-})
-
-router.post('/delete/:id', authAccess, getUserStatus, async (req, res) => {
-    try {
-        await deleteCube(req.params.id)
-        res.redirect(`/`)
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.get('/create', getUserStatus, (req, res) => {
+router.get('/create', authAccess, (req, res) => {
     res.render('create', {
         title: 'Create Cube | Cube Workshop',
     })
@@ -53,7 +17,7 @@ router.get('/create', getUserStatus, (req, res) => {
 router.post('/create', authAccess, async (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body
 
-    const token = req.cookies['aid']
+    const token = req.cookies[config.development.cookie]
     const decodedObject = jwt.verify(token, config.privateKey)
 
     const cube = new Cube({ name: name.trim(), description: description.trim(), imageUrl, difficulty: difficultyLevel, creatorId: decodedObject.userID })
@@ -64,13 +28,12 @@ router.post('/create', authAccess, async (req, res) => {
     } catch (err) {
         return res.render('create', {
             title: 'Create Cube | Cube Workshop',
-            isLoggedIn: req.isLoggedIn,
             error: 'Cube details is not valid'
         })
     }
 })
 
-router.get('/details/:id', getUserStatus, async (req, res) => {
+router.get('/details/:id', async (req, res) => {
     const cube = await getCubeWithAccessories(req.params.id)
 
     res.render('details', {
@@ -78,5 +41,44 @@ router.get('/details/:id', getUserStatus, async (req, res) => {
         ...cube,
     })
 })
+
+router.get('/edit/:id', authAccess, async (req, res) => {
+    const cube = await getCubeWithAccessories(req.params.id)
+
+    res.render('editCubePage', {
+        title: 'Edit cube| Cube Workshop',
+        cube,
+    })
+})
+
+router.post('/edit/:id', authAccess, async (req, res, next) => {
+    const { name, description, imageUrl, difficultyLevel } = req.body
+
+    try {
+        await editCube(req.params.id, { name, description, imageUrl, difficulty: difficultyLevel })
+        res.redirect(`/details/${req.params.id}`)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/delete/:id', authAccess, async (req, res) => {
+    const cube = await getCubeWithAccessories(req.params.id)
+    res.render('deleteCubePage', {
+        title: 'deleteCube | Cube Workshop',
+        ...cube,
+    })
+})
+
+router.post('/delete/:id', authAccess, async (req, res) => {
+    try {
+        await deleteCube(req.params.id)
+        res.redirect(`/`)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
 
 module.exports = router
